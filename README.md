9b6dbeca15cc7fea658c0203fd6874b1f37303de
JavierLuna <javierlunamolina@gmail.com>
2019-03-06 17:23:58 +0100

    Add drawer and README instructions

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 
 The framework for creating automatons for an already defined grammar
 (University project)
+
+# Installation
+
+`sudo apt-get install python-dev graphviz libgraphviz-dev pkg-config`
+
+Then, create a python 3.6 virtual environment:
+
+`python3.6 -m virtualenv env` && `source env/bin/activate`
+
+Finally, install all dependencies needed:
+
+`pip install transitions pygraphviz`
--- automaton4u/drawer/drawer.py
+++ automaton4u/drawer/drawer.py
@@ -2,11 +2,39 @@ from typing import Dict, List
 from automaton4u.parser.parser import AutomatonState, Parser
 from automaton4u.tokenizer import Tokenizer
 
+
+import os, sys, inspect, io
+
+cmd_folder = os.path.realpath(
+    os.path.dirname(
+        os.path.abspath(os.path.split(inspect.getfile(inspect.currentframe()))[0])))
+
+if cmd_folder not in sys.path:
+    sys.path.insert(0, cmd_folder)
+
+from transitions.extensions import MachineFactory
+
+
 """
 Example code:
     https://github.com/pytransitions/transitions/blob/master/examples/Graph%20MIxin%20Demo%20Nested.ipynb
     https://github.com/pytransitions/transitions/blob/master/examples/Graph%20MIxin%20Demo.ipynb
 """
+
+
+class Matter(object):
+    def is_hot(self):
+        return True
+
+    def is_too_hot(self):
+        return False
+
+    def show_graph(self, **kwargs):
+        # print(self.get_graph(**kwargs).string())
+        stream = io.BytesIO()
+        self.get_graph(**kwargs).draw(stream, prog='dot', format='png')
+        with open('illo.png', 'wb') as output_file:
+            output_file.write(stream.getvalue())
 
 def get_states(parent_state: AutomatonState, states: Dict[str, AutomatonState]) -> Dict[str, AutomatonState]:
     for state_name, state in parent_state.other_automaton_states.items():
@@ -36,8 +64,27 @@ def draw(automaton_state: AutomatonState):
     print(states)
     print(transitions)
 
+    graph_machine = MachineFactory.get_predefined(graph=True, nested=True)
+
+    model = Matter()
+    machine = graph_machine(model=model,
+                           states=states,
+                           transitions=transitions,
+                           auto_transitions=False,
+                           title="Automaton",
+                           show_conditions=True)
+    model.show_graph()
+
 
 grammar = 'A -> aB | bC \n C -> aC | bC | cB \n B -> bB | aD | a \n D -> aD | a | bB'
 tokens = Tokenizer(grammar).tokenize()
 result = Parser(tokens).parse()
 draw(result)
+
+
+
+
+
+
+
+
